a23adac30397fb6fa9b4304cd95c71d9f9d7716b
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-09-24 14:21:17 +0100

    Updates

---
+++ precache-manifest.b52d3677747076e85fb41cfcf44ed277.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ffad1faad41ec06b175a295f061828",
+    "url": "/Event-Scheduling/index.html"
+  },
+  {
+    "revision": "bacb1dfed08a3168a45c",
+    "url": "/Event-Scheduling/static/css/main.5707a463.chunk.css"
+  },
+  {
+    "revision": "8a963a655b28be51d758",
+    "url": "/Event-Scheduling/static/js/2.9224538a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Event-Scheduling/static/js/2.9224538a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacb1dfed08a3168a45c",
+    "url": "/Event-Scheduling/static/js/main.99da3c52.chunk.js"
+  },
+  {
+    "revision": "558f1bb5283f6d59b20a",
+    "url": "/Event-Scheduling/static/js/runtime-main.62d9dd0a.js"
+  },
+  {
+    "revision": "0a2729ccdfc647e773641179747d80e8",
+    "url": "/Event-Scheduling/static/media/namaste.0a2729cc.jpg"
+  },
+  {
+    "revision": "410e4675887623984a27e45c972d70a0",
+    "url": "/Event-Scheduling/static/media/perguntas_frequentes.410e4675.png"
+  }
+]);
